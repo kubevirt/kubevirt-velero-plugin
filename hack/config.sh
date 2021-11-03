966f15bb0506cbde41d99784d9ed65f20fcc9567
Bartosz Rybacki <brybacki@redhat.com>
2021-11-03 15:35:48 +0100

    bump velero-plugin-for-csi to v0.2.0
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- hack/config.sh
+++ hack/config.sh
@@ -61,4 +61,4 @@ kubectl="${_cli} --prefix ${PROVIDER} ssh node01 -- sudo kubectl --kubeconfig=/e
 DEPLOYMENT_TIMEOUT=600
 USE_CSI=${USE_CSI:-1}
 USE_RESTIC=${USE_RESTIC:-0}
-CSI_PLUGIN=${CSI_PLUGIN:-velero/velero-plugin-for-csi:main}
+CSI_PLUGIN=${CSI_PLUGIN:-velero/velero-plugin-for-csi:v0.2.0}
