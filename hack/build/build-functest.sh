45bb40f388ccecbf68ae6fbd9a26143eb7d7c847
Bartosz Rybacki <brybacki@redhat.com>
2021-12-09 16:27:06 +0100

    Simplify func-tests
    Travis no longer used
    
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- hack/build/build-functest.sh
+++ hack/build/build-functest.sh
@@ -17,26 +17,13 @@
 set -euo pipefail
 
 export PATH=$PATH:$HOME/gopath/bin
-JOB_TYPE="${JOB_TYPE:-}"
 
-if [ "${JOB_TYPE}" == "travis" ]; then
-    go get -v -t ./...
-    go install github.com/mattn/goveralls@latest
-    go install github.com/onsi/ginkgo/ginkgo@latest
-    go get -v github.com/onsi/gomega
-    go get -u github.com/evanphx/json-patch
-    go mod vendor
-    PACKAGE_PATH="pkg/"
-    mkdir -p coverprofiles
-    ginkgo -r -covermode atomic -outputdir=./coverprofiles -coverprofile=cover.coverprofile ${PACKAGE_PATH}
-else
-    test_path="tests"
-    (cd $test_path; go install github.com/onsi/ginkgo/ginkgo@latest)
-    (cd $test_path; GOFLAGS= go get github.com/onsi/gomega)
-    (cd $test_path; go mod  tidy; go mod vendor)
-    test_out_path=${test_path}/_out
-    mkdir -p ${test_out_path}
-    (cd $test_path; ginkgo build .)
-    mv ${test_path}/tests.test ${TESTS_OUT_DIR} 
-fi
+test_path="tests"
+(cd $test_path; go install github.com/onsi/ginkgo/ginkgo@latest)
+(cd $test_path; GOFLAGS= go get github.com/onsi/gomega)
+(cd $test_path; go mod  tidy; go mod vendor)
+test_out_path=${test_path}/_out
+mkdir -p ${test_out_path}
+(cd $test_path; ginkgo build .)
+mv ${test_path}/tests.test ${TESTS_OUT_DIR}
 
