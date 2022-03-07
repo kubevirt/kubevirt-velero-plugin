ab1b4f6ecbabcf18e0d2328cfcbcc09c4d31f5c3
Bartosz Rybacki <brybacki@redhat.com>
2022-03-07 16:37:35 +0100

    Emit junit report in artifacts dir
    Signed-off-by: Bartosz Rybacki <brybacki@redhat.com>

--- hack/run-ci.sh
+++ hack/run-ci.sh
@@ -15,9 +15,10 @@
 #limitations under the License.
 
 set -e
+readonly ARTIFACTS_PATH="${ARTIFACTS}"
 
 # Setup a cluster
 make cluster-up cluster-push-image cluster-sync CLUSTER_PREFIX='-p pull-kvp-functional-test'
 
 # Run the tests
-make test-functional CLUSTER_PREFIX='-p pull-kvp-functional-test'
+make test-functional CLUSTER_PREFIX='-p pull-kvp-functional-test' TEST_ARGS="--test-args=--junit-output=${ARTIFACTS_PATH}/junit.functest.xml"
