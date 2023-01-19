58370c4b4dda484363d41613b7fc4e5b0faecae3
Shelly Kagan <skagan@redhat.com>
2023-01-19 17:31:23 +0200

    Add tests manifests and functions to create them
    This manifests can be used to test any disaster recovery
    tools to make sure they work properly with kubevirt.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

---
+++ tests/framework/manifests_utils.go
@@ -0,0 +1,51 @@
+package framework
+
+func (f *Framework) CreateInstancetype() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/instancetype.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreatePreference() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/preference.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateConfigMap() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/configmap.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateSecret() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/secret.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateAccessCredentialsSecret() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/accessCredentialsSecret.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateBlankDataVolume() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/blank_datavolume.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateVMWithInstancetypeAndPreference() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/vm_with_instancetype_and_preference.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateVMWithDifferentVolumes() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/vm_with_different_volume_types.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateVMWithAccessCredentials() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/vm_with_access_credentials.yaml", "-n", f.Namespace.Name)
+	return err
+}
+
+func (f *Framework) CreateVMWithDVAndDVTemplate() error {
+	err := f.RunKubectlCommand("create", "-f", "manifests/vm_with_dv_and_dvtemplate.yaml", "-n", f.Namespace.Name)
+	return err
+}
