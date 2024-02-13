02fbd21bd1e07f97bbd25f21c6b2107ad7d5cecb
Shelly Kagan <skagan@redhat.com>
2024-02-13 23:30:30 +0200

    Generalize problematic tests (#212)
    * Generalize tests to check for specific resources in backup
    
    Instead of expecting a specific number of objects backed up
    we should check if the objects we care about were backed up,
    the check can also catch bugs that we could have missed in case
    the number didnt change but for some reason our desired object
    wasnt backed up.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>
    
    * Improve test to check specific pv and pod name
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>
    
    ---------
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- tests/framework/kubectl.go
+++ tests/framework/kubectl.go
@@ -1,11 +1,15 @@
 package framework
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
+	"github.com/pkg/errors"
 )
 
 //RunKubectlCommand runs a kubectl Cmd and returns output and err
@@ -44,4 +48,46 @@ func (f *Framework) RunKubectlCreateYamlCommand(yamlPath string) error {
 	fmt.Fprintf(GinkgoWriter, "INFO: Output from kubectl: %s\n", string(outBytes))
 
 	return err
+}
+
+// KubectlDescribeVeleroBackup execs to the velero pod and runs a describe command on the given
+// backup name including details of the resources that were backed up
+func (f *Framework) KubectlDescribeVeleroBackup(ctx context.Context, podName, backupName string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	kubeconfig := f.KubeConfig
+	path := f.KubectlPath
+	cmd := exec.CommandContext(ctx, path, "exec", "-it", podName, "-n", f.BackupNamespace, "--", "/velero", "backup", "describe", "--details", "-o", "json", backupName)
+	kubeconfEnv := fmt.Sprintf("KUBECONFIG=%s", kubeconfig)
+	cmd.Env = append(os.Environ(), kubeconfEnv)
+
+	stdoutPipe, err := cmd.StdoutPipe()
+	if err != nil {
+		return result, err
+	}
+
+	jsonBuf := make([]byte, 16*1024)
+	err = cmd.Start()
+	if err != nil {
+		return result, err
+	}
+
+	bytesRead, err := io.ReadFull(stdoutPipe, jsonBuf)
+	if err != nil && err != io.ErrUnexpectedEOF {
+		return result, err
+	}
+	if bytesRead == len(jsonBuf) {
+		return result, errors.New("json returned bigger than max allowed")
+	}
+
+	jsonBuf = jsonBuf[0:bytesRead]
+	err = cmd.Wait()
+	if err != nil {
+		return result, err
+	}
+	json.Unmarshal(jsonBuf, &result)
+	if err != nil {
+		return result, err
+	}
+
+	return result, nil
 }
