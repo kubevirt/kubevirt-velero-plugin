a96f3dfd23ad926042fd91821c740d2312458ed8
Lee Yarwood <lyarwood@redhat.com>
2025-03-20 08:57:26 +0000

    chore(hack): Update cluster-up from 2406170712-853264f to 2503182242-7ac16c8d
    This commit runs the hack/update-kubevirtci.sh to update the provided cluster-up version to one that supports KubeVirt v1.5.0.
    
    Signed-off-by: Lee Yarwood <lyarwood@redhat.com>

--- cluster-up/cluster/kind-1.31/provider.sh
+++ cluster-up/cluster/kind-1.31/provider.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DEFAULT_CLUSTER_NAME="vgpu"
+DEFAULT_CLUSTER_NAME="kind-1.31"
 DEFAULT_HOST_PORT=5000
 ALTERNATE_HOST_PORT=5001
 export CLUSTER_NAME=${CLUSTER_NAME:-$DEFAULT_CLUSTER_NAME}
@@ -14,9 +14,11 @@ else
 fi
 
 function set_kind_params() {
-    export KIND_VERSION="${KIND_VERSION:-0.18.0}"
-    export KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-kindest/node:v1.27.1@sha256:9915f5629ef4d29f35b478e819249e89cfaffcbfeebda4324e5c01d53d937b09}"
-    export KUBECTL_PATH="${KUBECTL_PATH:-/bin/kubectl}"
+    version=$(cat "${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/version")
+    export KIND_VERSION="${KIND_VERSION:-$version}"
+
+    image=$(cat "${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/image")
+    export KIND_NODE_IMAGE="${KIND_NODE_IMAGE:-$image}"
 }
 
 function configure_registry_proxy() {
@@ -32,24 +34,17 @@ function configure_registry_proxy() {
 }
 
 function up() {
-    # load the vfio_mdev module
-    /usr/sbin/modprobe vfio_mdev
-    
-    # print hardware info for easier debugging based on logs
-    echo 'Available cards'
-    ${CRI_BIN} run --rm --cap-add=SYS_RAWIO quay.io/phoracek/lspci@sha256:0f3cacf7098202ef284308c64e3fc0ba441871a846022bb87d65ff130c79adb1 sh -c "lspci -k | grep -EA2 'VGA|3D'"
-    echo ""
-
     cp $KIND_MANIFESTS_DIR/kind.yaml ${KUBEVIRTCI_CONFIG_PATH}/$KUBEVIRT_PROVIDER/kind.yaml
+    _add_kubeadm_cpu_manager_config_patch
     _add_extra_mounts
+    _add_extra_portmapping
+    export CONFIG_WORKER_CPU_MANAGER=true
     kind_up
 
     configure_registry_proxy
 
     # remove the rancher.io kind default storageClass
     _kubectl delete sc standard
-
-    ${KUBEVIRTCI_PATH}/cluster/$KUBEVIRT_PROVIDER/config_vgpu_cluster.sh
 
     echo "$KUBEVIRT_PROVIDER cluster '$CLUSTER_NAME' is ready"
 }
