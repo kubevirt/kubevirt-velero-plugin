549c457dacab8d40f4ae147b4787fc997bab9999
alromeros <alromero@redhat.com>
2025-04-10 21:22:05 +0200

    Get vm state storage class from KVP_STORAGE_CLASS instead of using default SC (#338)
    Signed-off-by: Alvaro Romero <alromero@redhat.com>

--- hack/cluster-deploy-prerequisites.sh
+++ hack/cluster-deploy-prerequisites.sh
@@ -49,14 +49,6 @@ until _kubectl wait -n kubevirt kv kubevirt --for condition=Available --timeout 
     sleep 1m
 done
 
-# Get the default storage class to patch vmStateStorageClass
-# TODO: Improve vmStateStorageClass handling
-DEFAULT_STORAGE_CLASS=$(_kubectl get storageclass -o jsonpath='{.items[?(@.metadata.annotations.storageclass\.kubernetes\.io/is-default-class=="true")].metadata.name}')
-if [ -n "$DEFAULT_STORAGE_CLASS" ]; then
-    # Patch kubevirt with VM state storage class
-    _kubectl patch -n kubevirt kubevirt kubevirt --type merge -p '{"spec": {"configuration": { "vmStateStorageClass": "'$DEFAULT_STORAGE_CLASS'" }}}'
-fi
-
 # Patch kubevirt with hotplug and persistent VM state feature gate enabled
 _kubectl patch -n kubevirt kubevirt kubevirt --type merge -p '{"spec": {"configuration": { "developerConfiguration": { "featureGates": ["HotplugVolumes", "VMPersistentState"] }}}}'
 
